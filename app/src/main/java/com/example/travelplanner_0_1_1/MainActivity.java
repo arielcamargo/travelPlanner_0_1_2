32efc19aa4e69cbca2d7717b48fc9aa24b321108
Jeffrey de Jesus <73007941+jeff-dejesus@users.noreply.github.com>
2020-12-08 21:14:48 -0800

    test commit

--- app/src/main/java/com/example/travelplanner_0_1_1/MainActivity.java
+++ app/src/main/java/com/example/travelplanner_0_1_1/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 
+//if you see this message, you've successfully downloaded changes
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button startProgram;
